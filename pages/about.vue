750d9571cd5c5c14dabfa8c8349e917ad93bceaa
JCrew <sanjeev.chavan.cft@jcrew.com>
2023-01-04 17:03:36 +0530

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -1,3 +1,43 @@
 <template>
-    <h1>About Cyriac</h1>
+    <section class="bg-white border-b py-8 mt-8">
+          <div class="container max-w-5xl mx-auto m-8">
+            <div class="flex flex-wrap flex-col-reverse sm:flex-row">
+              <div class="w-full sm:w-1/2 p-6 mt-6">
+                <img class="w-full md:w-4/5 z-50" src="~/assets/6.png" />
+              </div>
+              <div class="w-full sm:w-1/2 p-6 mt-6">
+                <div class="align-middle">
+                  <h3 class="text-3xl text-gray-800 font-bold leading-none my-4">
+                    About Us
+                  </h3>
+                  <p class="text-gray-600 mb-8"><b>Our history:</b> Humble beginnings and a desire to provide services that help businesses grow, lead us to start a dream. Initially we offered free services to help businesses to cope up from the devastating effects of the pandemic. Our dream journey was not an easy path, a path only a few chose.</p>
+                  <p class="text-gray-600 mb-8"><b>Our mission:</b> Businesses and marketing are loyal friends. We at Andre Digital Web Services bridge that gap and make it a strong lasting bond.	</p>
+                  <p class="text-gray-600 mb-8"><b>Our Vision:</b> Our future is to outgrow our own expectations and  to go beyond the expectation of our happy clients.</p>
+                  <p class="text-gray-600 mb-8"><b>Our values:</b> Wealth is the happiness of our clients. </p>
+                  <p class="text-gray-600 mb-8"><b>Our Market:</b> Our Services provide social media marketing to small and medium based compnanies based in Goa.</p>
+                </div>
+              </div>
+            </div>
+            <h2 class="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
+                Social Media Marketing
+            </h2>
+            <div class="w-full mb-4">
+              <div class="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
+            </div>
+            <div class="flex flex-wrap">
+              <div class="w-5/6 sm:w-1/2 p-6">
+                <h3 class="text-3xl text-gray-800 font-bold leading-none my-4">
+                    We manage Business page on Facebook, Instagram, design posts, videos and promote the page that brings in high growth in business and much more. 
+Web development</h3>
+                <p class="text-gray-600 mb-8">
+                    We develop apps that cater to the need of apple based clientle. We develop the best in terms of security and seo.
+                  <br />
+                  <br />
+
+                  High growth, High quality and High value to your Investment. An investment that is worth it. 
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
 </template>
