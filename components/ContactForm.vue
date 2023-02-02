95e9460d355779a5da0aa5f1cb6e04a6d2d31d79
andredigitalweb <124025260+andredigitalweb@users.noreply.github.com>
2023-02-02 22:47:40 +0530

    Update ContactForm.vue

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -24,7 +24,7 @@
           Don’t fill this out if you’re human: <input name="bot-field" />
         </label>
       </p>
-      <input type="hidden" name="form-name" value="contact-form" />
+      <input type="hidden" name="form-name" value="contact" />
       <div class="flex flex-wrap -mx-3 mb-6">
         <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
           <label class="block text-lg mb-2" for="business">
