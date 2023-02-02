ff34aa020f7541c8b94317c2bd13d4f43bd362d2
andredigitalweb <124025260+andredigitalweb@users.noreply.github.com>
2023-02-02 15:15:10 +0000

    revamp

--- pages/contact.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="py-8 mt-8 lg:py-16 mx-auto">
-    <ContactForm />
-    <TalkToUs />
-  </div>
-</template>
--- pages/iosplans.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div class="py-8 mt-8 lg:py-16 mx-auto">
-        <IOSPlans />
-        <TalkToUs />
-    </div>
-</template>
-  
